9a37f9d9f5babd471256f3c75f71d8dcd6b20ba7
Mitchell Sharma <sharmamitch@gmail.com>
2023-03-09 16:04:43 -0600

    Customized scrollbar with light and dark theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import useLocalStorage from 'use-local-storage';
-import Home from './pages/Home';
-import Navbar from './components/Navbar/Navbar';
-import './App.scss';
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import useLocalStorage from "use-local-storage";
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar/Navbar";
+import "./App.scss";
 
 function App() {
-  
-  const defaultDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
-  const [theme, setTheme] = useLocalStorage('theme', defaultDark ? 'dark' : 'light');
+  const defaultDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+  const [theme, setTheme] = useLocalStorage(
+    "theme",
+    defaultDark ? "dark" : "light"
+  );
 
   const switchTheme = () => {
-    const newTheme = theme === 'light' ? 'dark' : 'light';
+    const newTheme = theme === "light" ? "dark" : "light";
     setTheme(newTheme);
-  }
+    document.body.setAttribute("data-theme", newTheme);
+  };
+
+  React.useEffect(() => {
+    document.body.setAttribute("data-theme", theme);
+  }, [theme]);
 
   return (
     <div className="App" data-theme={theme}>
       <Router>
-        <Navbar switchTheme={switchTheme} theme={theme}/>
+        <Navbar switchTheme={switchTheme} theme={theme} />
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
