acce642a225ddd0d00c193e0357dbe2cfdfc1d16
Mitchell Sharma <sharmamitch@gmail.com>
2023-03-09 14:01:09 -0600

    Implemented mobile nav dropdown hamburger menu

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,42 +1,80 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import styles from "./Navbar.module.scss";
-import Button from '../Button/Button';
+import Button from "../Button/Button";
+// import Hamburger from "../Hamburger/Hamburger";
+import { Squash as HamburgerButton } from "hamburger-react";
 
-interface NavbarProps{
-    switchTheme: () => void;
-    theme: string;
+interface NavbarProps {
+  switchTheme: () => void;
+  theme: string;
 }
 
 const Navbar = (props: NavbarProps) => {
   const { switchTheme } = props;
+  const [isOpen, setOpen] = useState(false);
+
+  const paths = [
+    { link: "/", label: "Home" },
+    { link: "/about", label: "About" },
+    { link: "/demo", label: "Demo" },
+  ];
   return (
     <div>
-        <nav className={styles.navbar}>
-            <Link to="/" className={styles.logo}>Flash</Link>
-            <ul className={styles.navbar__list}>
-                <li className={styles.navbar__item}>
-                    <Link to="/" className={styles.navbar__link}>Home</Link>
-                </li>
-                <li className={styles.navbar__item}>
-                    <Link to="/about" className={styles.navbar__link}>About</Link>
-                </li>
-                <li className={styles.navbar__item}>
-                    <Link to="/demo" className={styles.navbar__link}>Demo</Link>
-                </li>
-                <div className={styles.navbar__item}>
-                    <Button label="Sign Up" link="/signup" />
-                </div>
-                <div className={styles.navbar__item}>
-                    <Button label="Login" link="/login" />
-                </div>
-                <div className={styles.navbar__item}>
-                    <Button label="Theme" onClick={switchTheme} />
-                </div>
-            </ul>
-        </nav>
+      <nav className={styles.navbar}>
+        <Link to="/" className={styles.logo}>
+          Flash
+        </Link>
+        <ul className={styles.navbar__list}>
+          {paths.map((path) => (
+            <li className={styles.navbar__item} key={path.link}>
+              <Link to={path.link} className={styles.navbar__link}>
+                {path.label}
+              </Link>
+            </li>
+          ))}
+          <div className={styles.navbar__item}>
+            <Button label="Sign Up" link="/signup" />
+          </div>
+          <div className={styles.navbar__item}>
+            <Button label="Login" link="/login" />
+          </div>
+          <div className={styles.navbar__item}>
+            <Button label="Theme" onClick={switchTheme} />
+          </div>
+        </ul>
+        <div className={styles.hamburgerbutton}>
+          <HamburgerButton
+            toggled={isOpen}
+            toggle={setOpen}
+            color={"var(--menu-font)"}
+          />
+        </div>
+      </nav>
+      {isOpen && (
+        <div className={styles.hamburger}>
+          <ul className={styles.hamburger__list}>
+            {paths.map((path) => (
+              <li className={styles.hamburger__item} key={path.link}>
+                <Link to={path.link} className={styles.navbar__link}>
+                  {path.label}
+                </Link>
+              </li>
+            ))}
+            <div className={styles.hamburger__item}>
+              <Button label="Sign Up" link="/signup" />
+            </div>
+            <div className={styles.hamburger__item}>
+              <Button label="Login" link="/login" />
+            </div>
+            <div className={styles.hamburger__item}>
+              <Button label="Theme" onClick={switchTheme} />
+            </div>
+          </ul>
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
