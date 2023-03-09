e90d970901ebd2f500fc539ce158fc47a6f5ed99
Mitchell Sharma <sharmamitch@gmail.com>
2023-03-09 12:32:10 -0600

    Added animation to hero section (via Framer Motion)

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -2,11 +2,21 @@ import React from "react";
 import styles from "./Hero.module.scss";
 import Flashcard from "../Flashcard/Flashcard";
 import Tilt from "react-parallax-tilt";
+import { motion } from "framer-motion";
 
 const Hero = () => {
   return (
-    <div className={styles.hero}>
-      <div className={styles.hero__text}>
+    <div className={styles.hero} id="home">
+      <motion.div
+        initial={{ opacity: 0, y: 50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{
+          duration: 0.25,
+          opacity: { ease: "easeIn" },
+          y: { ease: "easeOut" },
+        }}
+        className={styles.hero__text}
+      >
         <h1>Flash</h1>
         <h2>Effortless Flashcards</h2>
         <ul>
@@ -14,20 +24,26 @@ const Hero = () => {
           <li>100% Ad-free with No paywalls</li>
           <li>Import from Quizlet, Anki, Excel, and more</li>
         </ul>
-      </div>
-      <Tilt
-        tiltMaxAngleX={4}
-        tiltMaxAngleY={10}
-        scale={1.025}
-        tiltAngleYInitial={10}
-        tiltAngleXInitial={0}
+      </motion.div>
+      <motion.div
+        initial={{ opacity: 0, y: 50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.25, delay: 0.1 }}
       >
-        <Flashcard
-          front="What is the **best** flashcard app?"
-          back="***Flash***, a free and open source flashcard app, 
+        <Tilt
+          tiltMaxAngleX={4}
+          tiltMaxAngleY={10}
+          scale={1.025}
+          tiltAngleYInitial={10}
+          tiltAngleXInitial={0}
+        >
+          <Flashcard
+            front="What is the **best** flashcard app?"
+            back="***Flash***, a free and open source flashcard app, 
           created by [Mitchell Sharma](https://mitchellsharma.com)"
-        />
-      </Tilt>
+          />
+        </Tilt>
+      </motion.div>
     </div>
   );
 };
