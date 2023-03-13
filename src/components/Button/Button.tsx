a75234ab5b463dc3e8f3830fe70c590454474257
Mitchell Sharma <sharmamitch@gmail.com>
2023-03-13 17:38:31 -0500

    Updated button to handle empty link prop

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,18 +1,24 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 import styles from "./Button.module.scss";
 
 interface ButtonProps {
-    label: string;
-    link?: string;
-    onClick?: () => void;
+  label: string;
+  link?: string;
+  onClick?: () => void;
 }
 
 const Button = (props: ButtonProps) => {
-  const { label, link = '', onClick } = props;
-  return (
-    <Link to={link} onClick={onClick} className={styles.button}>{label}</Link>
-  )
-}
+  const { label, link, onClick } = props;
+  return link ? (
+    <Link to={link} onClick={onClick} className={styles.button}>
+      {label}
+    </Link>
+  ) : (
+    <div onClick={onClick} className={styles.button}>
+      {label}
+    </div>
+  );
+};
 
-export default Button
+export default Button;
