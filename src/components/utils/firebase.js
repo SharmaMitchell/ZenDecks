51871b436f980d4afc476a6c14412bcda41b874f
Mitchell Sharma <sharmamitch@gmail.com>
2023-03-14 14:28:04 -0500

    Firebase config setup

---
+++ src/components/utils/firebase.js
@@ -0,0 +1,22 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/storage";
+import "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCnZiVBYeqf7ntVQwy6yfidA8uF6nPP6Kw",
+  authDomain: "flashy-c6e62.firebaseapp.com",
+  projectId: "flashy-c6e62",
+  storageBucket: "flashy-c6e62.appspot.com",
+  messagingSenderId: "1033423310217",
+  appId: "1:1033423310217:web:b69543a350a0c47157e558",
+  measurementId: "G-0K50HJCSJD",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+export const storage = firebase.storage();
+
+export default firebase;
