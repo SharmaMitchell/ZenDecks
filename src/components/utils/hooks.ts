2627f127194caa0a28caa1631e7515cb5e27f797
Mitchell Sharma <sharmamitch@gmail.com>
2023-03-15 20:44:15 -0500

    Implemented pull for first 5 cards in each deck in useDecks hook

--- src/components/utils/hooks.ts
+++ src/components/utils/hooks.ts
@@ -28,22 +28,6 @@ export function useUserData() {
   return { user, username };
 }
 
-interface Deck {
-  id: string;
-  ref: any;
-  title: string;
-  description: string;
-  tags?: string[];
-  public: boolean;
-  authorName: string;
-  authorID: string;
-  cards?: any[];
-  rating?: number;
-  ratingCount?: number;
-  cardCount: number;
-  userCount?: number;
-}
-
 /* TODO: Only fetch data once per session, then store in redux store? */
 /* This didn't work before because you can't call a hook (useCollection) within a conditional or callback */
 // const FetchDecks = async () => {
@@ -76,20 +60,37 @@ interface Deck {
 // Custom hook to get deck data from firestore
 export function useDecks(): Deck[] {
   const decks = useSelector((state: RootState) => state.decks.decks);
+
   const [value, loading, error, snapshot] = useCollectionDataOnce(
     firestore.collection("decks").where("public", "==", true).limit(10) as any
   );
+
+  // Get the first 5 cards for each deck using a collectionGroup query
+  const [cardsValue, cardsLoading, cardsError, cardsSnapshot] =
+    useCollectionDataOnce(firestore.collectionGroup("cards").limit(5) as any);
+
   useEffect(() => {
-    if (snapshot) {
-      const docs = snapshot.docs.map((doc) => ({
-        id: doc.id,
-        ref: doc.ref,
-        ...doc.data(),
-      }));
+    if (cardsSnapshot && snapshot) {
+      const docs = snapshot.docs.map((doc) => {
+        console.log("cards: ", cardsSnapshot.docs[0].data());
+        console.log("parent: ", cardsSnapshot.docs[0].ref.parent?.parent?.id);
+        console.log("doc: ", doc.id);
+        const deckCards = cardsSnapshot.docs
+          .filter((cardDoc) => cardDoc.ref.parent?.parent?.id === doc.id)
+          .map((cardDoc) => cardDoc.data());
+        console.log("deckCards: ", deckCards);
+        return {
+          id: doc.id,
+          ref: doc.ref,
+          cards: deckCards,
+          ...doc.data(),
+        };
+      });
+
       store.dispatch(setDecks(docs));
       console.log("useDecks: ", docs);
     }
-  }, [snapshot]);
+  }, [cardsSnapshot, snapshot]);
 
   return decks;
 }
