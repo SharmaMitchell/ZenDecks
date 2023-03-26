33426813bf323c1599c7e251138df5e1cbb1c6cf
Mitchell Sharma <sharmamitch@gmail.com>
2023-03-26 12:36:28 -0500

    Study mode updates: Added swiper, tweaked styling

--- src/components/Study/Study.tsx
+++ src/components/Study/Study.tsx
@@ -3,18 +3,39 @@ import { useParams } from "react-router-dom";
 import { useDeck } from "../utils/hooks";
 import styles from "./Study.module.scss";
 import Flashcard from "../Flashcard/Flashcard";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination } from "swiper";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
 const Study = () => {
   const { deckId = "" } = useParams<{ deckId: string }>();
 
   const deck = useDeck(deckId);
 
+  if (!deck || !deck.cards) return null;
+
   return (
     <div className={styles.study}>
-      <h2>{deck?.title}</h2>
-      {deck?.cards?.map((card, index) => {
-        return <Flashcard key={index} front={card.front} back={card.back} />;
-      })}
+      <h2>{deck.title}</h2>
+      <div className={styles.study__cards}>
+        <Swiper
+          slidesPerView={1}
+          spaceBetween={60}
+          loop={false}
+          modules={[Navigation, Pagination]}
+          navigation={true}
+          pagination={true}
+          className={styles.swiper}
+        >
+          {deck.cards.map((card, index) => (
+            <SwiperSlide key={index} style={{ width: "auto" }}>
+              <Flashcard front={card.front} back={card.back} size="large" />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
     </div>
   );
 };
