bb197558ec94b98ea654ee8d2a78cddd1ea8cfae
Mitchell Sharma <sharmamitch@gmail.com>
2023-03-25 22:52:50 -0500

    WIP: Implemented Study component, calling useDeck(deckId)

--- src/components/Study/Study.tsx
+++ src/components/Study/Study.tsx
@@ -1,8 +1,28 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import { useDeck } from "../utils/hooks";
 import styles from "./Study.module.scss";
 
 const Study = () => {
-  return <div>Study</div>;
+  const { deckId = "" } = useParams<{ deckId: string }>();
+
+  const deck = useDeck(deckId);
+
+  return (
+    <div>
+      <h1>Study</h1>
+      <h2>{deck?.title}</h2>
+      <p>{deck?.description}</p>
+      {deck?.cards?.map((card, index) => {
+        return (
+          <div key={index}>
+            <h3>{card.front}</h3>
+            <p>{card.back}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Study;
