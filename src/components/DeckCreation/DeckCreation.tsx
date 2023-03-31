c86e7ad836d1e760d1e47afb7acb20bcb73b3b37
Mitchell Sharma <sharmamitch@gmail.com>
2023-03-31 18:46:57 -0500

    Added animation to "add card" button on mount

--- src/components/DeckCreation/DeckCreation.tsx
+++ src/components/DeckCreation/DeckCreation.tsx
@@ -140,9 +140,14 @@ const DeckCreation = () => {
           />
         ))}
       </div>
-      <div className={styles.deckcreation__addcard}>
+      <motion.div
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.2, delay: 0.2 }}
+        className={styles.deckcreation__addcard}
+      >
         <Button label="Add Card" onClick={addCard} />
-      </div>
+      </motion.div>
     </div>
   );
 };
