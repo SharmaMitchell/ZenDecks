c8f97a6b5c5abb8eb6754d9cf6a38c5078e36bbf
Mitchell Sharma <sharmamitch@gmail.com>
2023-03-30 23:15:07 -0500

    Styled card creation component to match Figma design

--- src/components/CardCreation/CardCreation.tsx
+++ src/components/CardCreation/CardCreation.tsx
@@ -19,34 +19,34 @@ const CardCreation = (props: CardCreationProps) => {
   return (
     <div className={styles.cardcreation}>
       <div className={styles.cardcreation__form}>
-        <div className={styles.cardcreation__front}>
-          <label className={styles.cardcreation__input__label} htmlFor="front">
-            Front
-          </label>
-          <textarea
-            rows={4}
-            name="front"
-            id="front"
-            placeholder="Front of the card"
-            className={styles.cardcreation__input}
-            value={front}
-            onChange={(e) => handleCardChange(index, "front", e.target.value)}
-          />
-        </div>
-        <div className={styles.cardcreation__back}>
-          <label className={styles.cardcreation__input__label} htmlFor="back">
-            Back
-          </label>
-          <textarea
-            rows={4}
-            name="back"
-            id="back"
-            placeholder="Back of the card"
-            className={styles.cardcreation__input}
-            value={back}
-            onChange={(e) => handleCardChange(index, "back", e.target.value)}
-          />
-        </div>
+        {/* <div className={styles.cardcreation__front}> */}
+        <label className={styles.cardcreation__input__label} htmlFor="front">
+          Front
+        </label>
+        <textarea
+          rows={4}
+          name="front"
+          id="front"
+          placeholder="Front of the card"
+          className={styles.cardcreation__input}
+          value={front}
+          onChange={(e) => handleCardChange(index, "front", e.target.value)}
+        />
+        {/* </div> */}
+        {/* <div className={styles.cardcreation__back}> */}
+        <label className={styles.cardcreation__input__label} htmlFor="back">
+          Back
+        </label>
+        <textarea
+          rows={4}
+          name="back"
+          id="back"
+          placeholder="Back of the card"
+          className={styles.cardcreation__input}
+          value={back}
+          onChange={(e) => handleCardChange(index, "back", e.target.value)}
+        />
+        {/* </div> */}
       </div>
       <div className={styles.cardcreation__preview}>
         <Flashcard front={front} back={back} size={"small"} />
