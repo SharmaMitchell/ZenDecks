06861ab206c686a0c968550a62f58b48c719a97d
Mitchell Sharma <sharmamitch@gmail.com>
2023-03-30 23:28:38 -0500

    Fixed deck creation title margin

--- src/components/DeckCreation/DeckCreation.tsx
+++ src/components/DeckCreation/DeckCreation.tsx
@@ -43,6 +43,7 @@ const DeckCreation = () => {
         initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.2 }}
+        className={styles.deckcreation__title}
       >
         Create Deck
       </motion.h2>
