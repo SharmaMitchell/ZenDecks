6eabc5652436bf9b8d101d4355422a7dda35397f
Mitchell Sharma <sharmamitch@gmail.com>
2023-04-08 16:08:48 -0500

    Revised dropdown menu animation Removed y-axis animation, reduced fade-in time

--- src/components/DropdownMenu/DropdownMenu.tsx
+++ src/components/DropdownMenu/DropdownMenu.tsx
@@ -31,9 +31,9 @@ export const DropdownMenu = ({
       <ToggleButtonWrapper />
       {isOpen && (
         <motion.div
-          initial={{ opacity: 0, y: +20 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.15 }}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.1 }}
           className={
             styles.dropdown__content +
             " " +
